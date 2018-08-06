fdce1d055c18d1f9ff60f4eb904e1163af116ae7
pborawski <pborawski@xcode.pl>
2018-08-06 22:02:34 +0200

    init i interfejs

---
+++ src/DumbContainer/IDumbContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DumbContainer
+{
+    public interface IDumbContainer
+    {
+        object GetInstance(Type type);
+        T GetInstance<T>();
+
+        void Register(Type service, Type implementation);
+        void Register<TService, TImplementation>();
+    }
+}
